ecbe92f70f33cdaf14b6f95851d32a8fb370dc08
Welsh, Mark <mark.welsh@accenture.com>
2015-02-05 12:14:49 +0000

    Added app first version

---
+++ app.js
@@ -0,0 +1,54 @@
+var http      = require('http');
+var mongoose  = require('mongoose');
+var express   = require('express');
+
+var app    = express(); var db;
+
+var config = {
+    "USER"    : "",
+    "PASS"    : "",
+    "HOST"    : "ec2-54-186-25-151.us-west-2.compute.amazonaws.com",
+    "PORT"    : "27017",
+    "DATABASE" : "my_example"
+};
+
+var dbPath  =   "mongodb://"+config.USER + ":"+
+                config.PASS + "@"+
+                config.HOST + ":"+
+                config.PORT + "/"+
+                config.DATABASE;
+
+var standardGreeting = 'Hello World!';
+
+var greetingSchema = mongoose.Schema({   sentence: String });
+var Greeting= mongoose.model('GreetingreetingSchema);
+
+db = mongoose.connect(dbPath);
+
+mongoose.connection.once('openunction() { 
+    var greeting;
+    Greeting.find( function(err, greetings){
+        if( !greetings ){
+            greeting = new Greeting({ sentence: standardGreeting });
+            greeting.save();
+        }
+    });
+});
+
+app.get('/', function(req, res){
+    Greeting.findOne(function (err, greeting) {
+        res.send(greeting.sentence);
+    });
+});
+
+app.use(function(err, req, res, next){
+    if (req.xhr) {
+        res.send(500, 'Something went wrong!');
+    } else {
+        next(err);
+    }
+});
+
+console.log('starting Express (NodeJS) Web server');
+app.listen(8080);
+console.log('Webserverlistening on port 8080');
